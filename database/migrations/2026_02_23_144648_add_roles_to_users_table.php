a9d4b1ab414494072fa8ba0add9f4b7376473ee1
Yassine Sekkah <yassinesekkah11@gmail.com>
2026-02-23 20:51:29 +0000

    EAS-12 Middleware to block banned users

--- database/migrations/2026_02_23_144648_add_roles_to_users_table.php
+++ database/migrations/2026_02_23_144648_add_roles_to_users_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->boolean('is_admin')->default(false);
+            $table->boolean('is_banned')->default(false);
+            $table->integer('reputation')->default(0);
         });
 
     }
